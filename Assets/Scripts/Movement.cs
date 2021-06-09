2d9b561d3a737aa7240a86d392ea9e95ddb16fef
El-Parto <luc980@y7mail.com>
2021-06-09 17:39:41 +1000

    error handling

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,8 +1,9 @@
+
+/*
 using UnityEngine;
 using UnityEngine.InputSystem;
 // handles the player moving. Not to be confused with Player manager, that handles the DATA so that the this script can use it.
 
-/*
 [RequireComponent(typeof(CapsuleCollider), typeof(Rigidbody))]
 [RequireComponent(typeof(PlayerInput))]
 public class Movement : Scoring
